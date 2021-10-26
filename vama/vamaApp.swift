f8de12088d3f47ccaf990d8367b69d66d95af2d2
archissoft <archissoft@gmail.com>
2021-10-26 16:26:51 +0300

    Initial Commit

---
+++ vama/vamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  vamaApp.swift
+//  vama
+//
+//  Created by Vadim Gojan on 26.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct vamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
